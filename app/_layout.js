12501f3e5c0c3fca3376d91f6d4b232292a9dce7
Alisher Ortiqov <mcpeblocker@gmail.com>
2024-05-19 00:11:05 +0900

    [feat] toast init

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,12 @@
 import { Slot } from "expo-router";
 import { SessionProvider } from "../contexts/auth.ctx";
+import Toast from "react-native-toast-message";
 
 export default function Root() {
   return (
     <SessionProvider>
       <Slot />
+      <Toast />
     </SessionProvider>
   );
 }
--- app/main/index.js
+++ app/main/index.js
@@ -1,11 +1,21 @@
-import { Button, StyleSheet, View } from "react-native";
+import { Button, StyleSheet, View, Text } from "react-native";
 import { useSession } from "../../contexts/auth.ctx";
+import Toast from "react-native-toast-message";
 
 export default function Page() {
   const { signOut } = useSession();
 
   const logOut = () => {
     // Implement log out
+    Toast.show({
+      type: "success",
+      text1: "Logged Out",
+      text2: "You have been logged out successfully.",
+      position: "bottom",
+      onPress: () => {
+        Toast.hide();
+      },
+    });
     signOut();
   };
 
