d93a93b29ac5e8c6ced861c744a400c5f44ec1cb
Alisher Ortiqov <mcpeblocker@gmail.com>
2024-05-22 16:23:47 +0900

    [feat] search page & generalized BookItem

--- app/(tabs)/home/index.js
+++ app/(tabs)/home/index.js
@@ -1,17 +1,8 @@
 import { useEffect, useState } from "react";
-import dayjs from "dayjs";
-import { Entypo, Ionicons, Octicons } from "@expo/vector-icons";
-import {
-  View,
-  Text,
-  FlatList,
-  Pressable,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+import { Octicons } from "@expo/vector-icons";
+import { View, Text, FlatList, Pressable } from "react-native";
 import { genres } from "../../../api/constants";
-import { getAvatarUrl } from "../../../components/users/Avatar";
-import { router } from "expo-router";
+import BookItem from "../../../components/book/BookItem";
 import { useTranslation } from "react-i18next";
 
 export default function Tab() {
@@ -186,120 +177,6 @@ function NoBooksFound() {
         {i18n.t("feed.empty-feedback")}
       </Text>
     </View>
-  );
-}
-
-function BookItem({ book }) {
-  const isAvailable = book.status === "AVAILABLE" && !book.exchangeId;
-  const isRequested = book.status === "AVAILABLE" && book.exchangeId;
-  const isReserved = book.status === "RESERVED" && book.exchangeId;
-  const i18n = useTranslation();
-
-  const handleRequest = () => {
-    console.log("Requesting book...");
-    console.log(book.bookId);
-  };
-
-  const handleChatroom = () => {
-    router.push({
-      pathname: "message/chat",
-      params: { exchangeId: book.exchangeId },
-    });
-  };
-
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        gap: 16,
-        padding: 4,
-      }}
-    >
-      {/* Cover image */}
-      <Image
-        src={getAvatarUrl(book.cover)}
-        style={{ width: 75, height: 100 }}
-      />
-      {/* Book info */}
-      <View style={{ gap: 4, justifyContent: "space-around" }}>
-        {/* Title */}
-        <Text style={{ fontWeight: "bold", fontSize: 24 }}>{book.title}</Text>
-        {/* Author */}
-        <Text style={{ fontSize: 14 }}>{book.author}</Text>
-        {/* Descriptors */}
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          {/* Date posted to the system */}
-          <Text style={{ color: "#6E7A9F" }}>
-            {dayjs(book.createdAt).format("MM/DD")}
-          </Text>
-          <Entypo name="dot-single" size={12} color="#6E7A9F" />
-          {/* Genre */}
-          <Text style={{ color: "#6E7A9F" }}>{book.genre}</Text>
-        </View>
-        {/* Status */}
-        {isAvailable && (
-          <ActionButton
-            label={i18n.t("feed.available")}
-            bgColor={"#DEE1EB"}
-            textColor={"black"}
-            onPress={handleRequest}
-          />
-        )}
-        {isRequested && (
-          <ActionButton
-            label={i18n.t("feed.requested")}
-            icon={<Ionicons name="checkmark-outline" size={14} color="white" />}
-            bgColor={"#6E7A9F"}
-            textColor={"white"}
-            disabled={true}
-          />
-        )}
-        {isReserved && (
-          <ActionButton
-            label={i18n.t("feed.reserved")}
-            icon={
-              <Ionicons
-                name="chatbubble-ellipses-outline"
-                size={14}
-                color="white"
-              />
-            }
-            bgColor={"#47547a"}
-            textColor={"white"}
-            onPress={handleChatroom}
-          />
-        )}
-      </View>
-    </View>
-  );
-}
-
-function ActionButton({
-  bgColor,
-  textColor,
-  label,
-  icon,
-  onPress = () => {},
-  disabled = false,
-}) {
-  return (
-    <TouchableOpacity
-      style={{
-        backgroundColor: bgColor,
-        paddingHorizontal: 16,
-        paddingVertical: 8,
-        borderRadius: 40,
-        flexDirection: "row",
-        gap: 4,
-        justifyContent: "space-evenly",
-        alignItems: "center",
-      }}
-      disabled={disabled}
-      onPress={onPress}
-    >
-      <Text style={{ color: textColor }}>{label}</Text>
-      {icon && icon}
-    </TouchableOpacity>
   );
 }
 
--- app/(tabs)/home/search.js
+++ app/(tabs)/home/search.js
@@ -1,14 +1,183 @@
-import { View, Text } from 'react-native';
+import { Ionicons, Octicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import {
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import BookItem from "../../../components/book/BookItem";
 
 export default function SearchPage() {
+  const mockResults = [
+    {
+      bookId: "66309f8691d019ed240c646f",
+      title: "JUSTICE",
+      author: "Michael J.Sandel",
+      genre: "Philosophy",
+      cover: "028e43d0783530373609309002fa405e.png",
+      createdAt: "2024-02-28T12:42:18.179+00:00",
+      status: "AVAILABLE",
+    },
+    {
+      bookId: "66309f8691d019ed240c646f",
+      title: "JUSTICE",
+      author: "Michael J.Sandel",
+      genre: "Philosophy",
+      cover: "028e43d0783530373609309002fa405e.png",
+      createdAt: "2024-02-28T12:42:18.179+00:00",
+      status: "AVAILABLE",
+    },
+    {
+      bookId: "66309f8691d019ed240c646f",
+      title: "JUSTICE",
+      author: "Michael J.Sandel",
+      genre: "Philosophy",
+      cover: "028e43d0783530373609309002fa405e.png",
+      createdAt: "2024-02-28T12:42:18.179+00:00",
+      status: "AVAILABLE",
+    },
+  ];
+
+  const { i18n } = useTranslation();
+
+  const [text, setText] = useState("");
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleTextChange = async (text) => {
+    setText(text);
+  };
+
+  const handleSearch = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+      const randomLength = Math.floor(Math.random() * 3);
+      setResults(mockResults.slice(0, randomLength));
+    }, 1000);
+  };
+
   return (
-    <View style={{ alignItems: 'center', justifyContent: "center", height: 100, paddingVertical: 12 }}>
-      {/* Content */}
-      <View>
-        {/* Genres */}
-        <Text>Search page</Text>
-        {/* List of books */}
+    <View
+      style={{
+        paddingVertical: 40,
+        backgroundColor: "white",
+        flex: 1,
+      }}
+    >
+      {/* Search bar */}
+      <View
+        style={{
+          // flexShrink: 1,
+          flexDirection: "row",
+          backgroundColor: "white",
+          justifyContent: "space-between",
+          padding: 8,
+          gap: 4,
+        }}
+      >
+        <TouchableOpacity
+          onPress={router.back}
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Ionicons name="chevron-back" size={24} color="black" />
+        </TouchableOpacity>
+        {/* Input */}
+        <TextInput
+          placeholder={i18n.t("search.placeholder")}
+          style={{
+            borderWidth: 1,
+            borderRadius: 20,
+            borderColor: "#DEE1EB",
+            paddingHorizontal: 12,
+            flexGrow: 1,
+            maxWidth: "",
+          }}
+          clearButtonMode="while-editing"
+          inputMode="search"
+          value={text}
+          onChangeText={handleTextChange}
+          onSubmitEditing={handleSearch}
+        />
+        {/* Send button */}
+        <TouchableOpacity
+          onPress={handleSearch}
+          style={{
+            backgroundColor: "#40B250",
+            borderRadius: 50,
+            padding: 8,
+            justifyContent: "center",
+            alignItems: "center",
+            opacity: !text || isLoading ? 0.6 : 1,
+          }}
+          disabled={!text || isLoading}
+        >
+          {isLoading ? (
+            <SpinningIcon />
+          ) : (
+            <Ionicons name="search" size={16} color="white" />
+          )}
+        </TouchableOpacity>
       </View>
+      {/* Content */}
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            paddingHorizontal: 12,
+            paddingVertical: 6,
+            backgroundColor: "#F2F3F7",
+            borderRadius: 4,
+            fontSize: 16,
+            textAlign: "center",
+            color: "#6E7A9F",
+          }}
+        >
+          {i18n.t("search.results", { count: results.length })}
+        </Text>
+      </View>
+      <FlatList
+        contentContainerStyle={{
+          flexGrow: 1,
+          padding: 24,
+          gap: 12,
+        }}
+        ListEmptyComponent={NoResults}
+        data={results}
+        keyExtractor={(_, i) => i}
+        renderItem={({ item }) => <BookItem book={item} />}
+      />
     </View>
   );
+}
+
+function NoResults() {
+  const i18n = useTranslation();
+  return (
+    <View style={{ alignItems: "center", gap: 8, paddingVertical: 16 }}>
+      <Octicons name="inbox" size={36} color="black" />
+      <Text style={{ fontWeight: "bold", fontSize: 18 }}>
+        {i18n.t("search.empty")}
+      </Text>
+      <Text style={{ fontSize: 16, color: "#6E7A9F" }}>
+        {i18n.t("search.empty-feedback")}
+      </Text>
+    </View>
+  );
+}
+
+function SpinningIcon() {
+  return <Ionicons name="refresh" size={16} color="white" />;
 }
